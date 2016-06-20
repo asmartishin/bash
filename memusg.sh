5cb90eaac1a77d9bc3da533c7667467c99034c5b
Alexey Martishin <alex.martishin@yandex.ru>
2016-06-20 18:01:47 +0300

    Create memusg.sh

---
+++ memusg.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -um
+
+[[ $# -gt 0 ]] || { sed -n '2,/^#$/ s/^# //p' <"$0"; exit 1; }
+
+pgid=$(ps -o pgid= $$)
+if [[ "$pgid" == "$(ps -o pgid= $(ps -o ppid= $$))" ]]; then
+    cmd=
+    set -- "$0" "$@"
+    for a; do cmd+="'${a//"'"/"'\\''"}' "; done
+    exec bash -i -c "$cmd"
+fi
+
+case $(uname) in
+    Darwin|*BSD) sizes() { /bin/ps -o rss= -g $1; } ;;
+    Linux) sizes() { /bin/ps -o rss= -$1; } ;;
+    *) echo "$(uname): unsupported operating system" >&2; exit 2 ;;
+esac
+
+(
+peak=0
+while sizes=$(sizes $pgid)
+do
+    set -- $sizes
+    sample=$((${@/#/+}))
+    let peak="sample > peak ? sample : peak"
+    sleep 0.1
+done
+echo "memusg: peak=$peak" >&2
+) &
+monpid=$!
+
+exec "$@"
